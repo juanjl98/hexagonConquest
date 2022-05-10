990d64f6cca5f61ca9a824106de73c4b688fa07d
juanjl98 <juanjimenezleon98@uma.es>
2022-05-10 16:22:07 +0200

    primeros atisbos

--- hexagonconquest/src/App.js
+++ hexagonconquest/src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
 function App() {
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="fondo">
+      Proyecto HexagonConquest
+      <Tablero/>
     </div>
   );
 }
 
 export default App;
+
+const Tablero = () => {
+
+  return (
+    <div className="tablero">
+      <Casillas/>
+    </div>
+  )
+}
+
+const Casillas = () => {
+  return (
+    <div className ="filaCasillas">
+    <Casilla/><Casilla/><Casilla/><Casilla/><Casilla/>
+    </div>
+  )
+}
+
+const Casilla = () => {
+
+  const casilla = {
+    aspectRatio: `1 / ${Math.sqrt(3)  / 2}`,
+    backgroundColor: "lightgreen",
+    clipPath: "polygon(25% 0, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)",
+    width: `${window.innerWidth/6}px`,
+    position: "relative",
+    border: "0",
+    transform: "rotate(30deg)",
+    display: "inline-block",
+  }
+
+  return (
+    <button className="casilla" style={{}}></button>
+  )
+}
